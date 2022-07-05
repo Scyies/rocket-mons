2ffa3ab17b56eee7e25f58103a45932af8a39d5f
Scyies <scyies3@gmail.com>
2022-07-05 19:42:42 -0300

    messagePage

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Adopt } from "./pages/Adopt";
 import { Home } from './pages/Home'
 import { LogIn } from "./pages/LogIn";
+import { MessagePage } from "./pages/MessagePage";
 import { SignIn } from "./pages/SignIn";
 
 export function Router() {
@@ -11,6 +12,7 @@ export function Router() {
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/log-in" element={<LogIn />} />
       <Route path="/adopt" element={<Adopt />} />
+      <Route path="/adoption-message" element={<MessagePage />} />
     </Routes>
   )
 }
---
+++ src/pages/MessagePage.tsx
@@ -0,0 +1,41 @@
+import { Button } from "../components/Button";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import { Input } from "../components/Inputs";
+
+export function MessagePage() {
+  return (
+    <div>
+      <Header />
+
+      <h2 className="text-blue-500 px-16 pt-14 pb-6 text-center">
+        Envie uma mensagem para a pessoa ou instituição que está cuidando do animal:
+      </h2>
+
+      <form action=""
+        className="flex flex-col justify-center mx-6 bg-gray-300 rounded-md place-items-center py-8 mb-4"
+      >
+        <Input name="Nome" id="nome" holder="Insira seu nome completo" type="text" />
+        <Input name="Telefone" id="telefone" holder="Insira seu telefone e/ou whatsapp" type="tel" />
+        <Input name="Nome do animal" id="nome-animal" holder="Por qual animal você se interessou?" type="text" />
+
+        <div className="flex flex-col">
+          <label htmlFor="message"
+            className="text-blue-500 pb-4 font-semibold"
+          >
+            Mensagem
+          </label>
+          <textarea name="message" id="message" 
+            cols={15} rows={5} 
+            placeholder="Escreva sua mensagem"
+            className="w-[336px] rounded-md mb-8 pl-4 pt-4"
+          ></textarea>
+        </div>
+
+        <Button name="Enviar" />
+      </form>
+
+      <Footer />
+    </div>
+  )
+}
