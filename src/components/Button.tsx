dfe3f6bbb57790a86a464accb68248e064a64595
Scyies <scyies3@gmail.com>
2022-07-25 20:45:25 -0300

    btn min size

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,7 @@ interface PropsName {
 export function Button(props: PropsName) {
   return (
       <button 
-        className={classNames("bg-red-500 py-2 px-2 rounded-md shadow-md w-full max-w-[180px] md:max-w-[340px] lg:max-w-[360px] text-white font-semibold hover:bg-red-400 transition-colors z-20",
+        className={classNames("bg-red-500 py-2 px-2 rounded-md shadow-md w-full min-w-[100px] max-w-[180px] md:max-w-[340px] lg:max-w-[360px] text-white font-semibold hover:bg-red-400 transition-colors z-20",
           {'disabled:opacity-50': props.loading,
             '': !props.loading
           }
