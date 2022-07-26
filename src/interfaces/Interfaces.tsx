e83c194b03f1c0a086e5d9b08f363748f1347912
Scyies <scyies3@gmail.com>
2022-07-26 16:53:18 -0300

    interfaces

---
+++ src/interfaces/Interfaces.tsx
@@ -0,0 +1,29 @@
+import { Timestamp } from "firebase/firestore";
+
+export interface UserProfile {
+  avatarURL: string;
+  bioMessage: string;
+  city: string;
+  email: string;
+  name: string;
+  telNumber: string;
+}
+
+export interface Animals {
+  age?: string;
+  behavior?: string;
+  imageURL?: string;
+  location?: string;
+  name?: string;
+  size?: string;
+  id?: string;
+}
+
+export interface AdoptionMessage {
+  animalName: string;
+  created_at: Timestamp;
+  email: string;
+  message: string;
+  name: string;
+  telNumber: string;
+}
